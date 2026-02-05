69a1121e0ac4479bc509d83c3c3f1330d9e82c95
César Augusto <cesar.augusto.rj1@gmail.com>
2026-02-04 21:13:11 -0300

    first commit

---
+++ src/main/java/br/com/cesaravb/zabbixincident/ZabbixIncidentServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.cesaravb.zabbixincident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZabbixIncidentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZabbixIncidentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cesaravb/zabbixincident/ZabbixIncidentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesaravb.zabbixincident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZabbixIncidentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
