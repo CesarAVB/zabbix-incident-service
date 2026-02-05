ccad2f62b9194142b93c528fa50921472bc2ba5b
César Augusto <cesar.augusto.rj1@gmail.com>
2026-02-05 00:53:45 -0300

    feat: substitui ZabbixIncidentServiceApplication por Startup como ponto de entrada da aplicação

--- src/main/java/br/com/cesaravb/zabbixincident/Startup.java
+++ src/main/java/br/com/cesaravb/zabbixincident/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZabbixIncidentServiceApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZabbixIncidentServiceApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
