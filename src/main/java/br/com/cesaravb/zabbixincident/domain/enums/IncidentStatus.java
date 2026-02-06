3d03145e2c5b4a7849d9babb797f368ba7b304f8
César Augusto <cesar.augusto.rj1@gmail.com>
2026-02-06 02:21:27 -0300

    feat: adiciona enums IncidentStatus e SeverityLevel para gerenciamento de incidentes

---
+++ src/main/java/br/com/cesaravb/zabbixincident/domain/enums/IncidentStatus.java
@@ -0,0 +1,8 @@
+package br.com.cesaravb.zabbixincident.domain.enums;
+
+public enum IncidentStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
---
+++ src/main/java/br/com/cesaravb/zabbixincident/domain/enums/SeverityLevel.java
@@ -0,0 +1,9 @@
+package br.com.cesaravb.zabbixincident.domain.enums;
+
+public enum SeverityLevel {
+    CRITICAL,
+    HIGH,
+    MEDIUM,
+    LOW,
+    INFO
+}
